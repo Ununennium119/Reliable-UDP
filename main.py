dd30d7debe5e82806d88d242e86d8b5dc9614932
Mohammad Sadeghi <moh02sadeghi@gmail.com>
2024-01-05 03:40:40 +0330

    It's working

--- main.py
+++ main.py
@@ -4,82 +4,103 @@ import time
 
 
 def main():
-    pass
+    application = Application(('localhost', 4444), ('localhost', 5555))
+    application.run()
 
 
 class Application:
-    def __init__(self, listen_port: int, send_port: int):
-        self.listen_port = listen_port
+    def __init__(self, listen_address: tuple[str, int], send_address: tuple[str, int]):
         self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.listen_socket.bind(('localhost', listen_port))
-        self.listen_seq_number = -1
+        self.listen_socket.bind(listen_address)
+        self.listen_seq_number = 0
         self.listen_data = []
 
-        self.send_port = send_port
+        self.send_address = send_address
         self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.send_socket.bind(('localhost', send_port))
-        self.send_seq_number = 0
+        self.send_base_seq_number = 1
+        self.send_current_seq_number = 1
+        self.send_window_size = 1024
         self.send_data = []
 
+        self.lock = threading.Lock()
+
     def run(self):
-        listen_thread = threading.Thread(target=self.listen)
-        send_thread = threading.Thread(target=self.send_data)
+        data_file = open('data.txt')
+        self.send_data = [line.strip() for line in data_file.readlines()]
+        # read_input_thread = threading.Thread(target=self._read_input)
+        listen_thread = threading.Thread(target=self._listen)
+        send_thread = threading.Thread(target=self._send)
+        # read_input_thread.start()
         listen_thread.start()
         send_thread.start()
+        # read_input_thread.join()
+        listen_thread.join()
+        send_thread.join()
 
-    def listen(self):
+    def _listen(self):
         while True:
-            data, addr = self.listen_socket.recvfrom(1024)
-            is_ack = bool(data[0])
-            seq_number = int(data[1:4])
+            try:
+                data, addr = self.listen_socket.recvfrom(1024)
+                is_ack = bool(data[0])
+                seq_number = int.from_bytes(data[1:5], 'big')
+                print(f'Something received: {data}')
 
-            if is_ack:
-                pass
-            else:
-                if seq_number != self.listen_seq_number + 1:
-                    # Send duplicate ACK
-                    is_ack = bytes(True)
-                    seq_number = self.listen_seq_number.to_bytes(4)
+                if is_ack:
+                    print(f'Ack received: {seq_number}')
+                    self.lock.acquire()
+                    if seq_number != self.send_base_seq_number:
+                        self.send_base_seq_number = seq_number + 1
+                        self.send_current_seq_number = seq_number + 1
+                    else:
+                        self.send_base_seq_number += 1
+                    self.lock.release()
+                else:
+                    if seq_number != self.listen_seq_number + 1:
+                        # Send duplicate ACK
+                        print(f'Sending duplicate ack: {self.listen_seq_number}')
+                        is_ack = int(True).to_bytes(1, 'big')
+                        seq_number = self.listen_seq_number.to_bytes(4, 'big')
+                        ack = is_ack + seq_number
+                        self.send_socket.sendto(ack, self.send_address)
+                        continue
+
+                    # Read data
+                    data = data[5:].decode()
+                    self.listen_data.append(data)
+                    print(f'Data received: {data}, seq_number: {seq_number}')
+
+                    # Send new ACK
+                    is_ack = int(True).to_bytes(1, 'big')
+                    seq_number = seq_number.to_bytes(4, 'big')
                     ack = is_ack + seq_number
-                    self.listen_socket.send(ack)
-                    continue
+                    self.send_socket.sendto(ack, addr)
 
-                # Read data
-                data = data[4:].decode()
-                self.listen_data.append(data)
-                print(f'Data received: {data}')
+                    self.listen_seq_number += 1
+            except Exception as e:
+                print(e)
 
-                # Send new ACK
-                is_ack = bytes(True)
-                seq_number = seq_number.to_bytes(4)
-                ack = is_ack + seq_number
-                self.listen_socket.send(ack)
-
-    def receive_ack(self):
+    def _send(self):
         while True:
-            data, addr = self.send_socket.recvfrom(1024)
-            is_ack = bool(data[0])
-            seq_number = int(data[1:4])
-            if not is_ack:
+            self.lock.acquire()
+            current_seq_number = self.send_current_seq_number
+            self.lock.release()
+            if len(self.send_data) <= current_seq_number - 1:
+                time.sleep(100)
                 continue
 
-            self.send_seq_number = seq_number + 1
+            print(f'Sending data with seq number: {current_seq_number}')
+            is_ack = int(False).to_bytes(1, 'big')
+            seq_number = self.send_current_seq_number.to_bytes(4, 'big')
+            data = is_ack + seq_number + self.send_data[current_seq_number - 1].encode()
+            self.send_socket.sendto(data, self.send_address)
+            self.lock.acquire()
+            self.send_current_seq_number += 1
+            self.lock.release()
 
-    def read_input(self):
-        while True:
-            data = input()
-            self.send_data.append(data)
-
-    def send(self):
-        while True:
-            if len(self.send_data) <= self.send_seq_number:
-                time.sleep(500)
-
-            is_ack = bytes(False)
-            seq_number = self.send_seq_number.to_bytes(4)
-            data = is_ack + seq_number + self.send_data[self.send_seq_number].encode()
-            self.send_socket.send(data)
-            self.send_seq_number += 1
+    # def _read_input(self):
+    #     while True:
+    #         data = input()
+    #         self.send_data.append(data)
 
 
 if __name__ == '__main__':
